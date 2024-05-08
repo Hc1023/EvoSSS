3be6da59935077fddef4fca1367374ed41e87c71
HappySisi <52774929+Hc1023@users.noreply.github.com>
2024-05-08 20:41:04 +0800

    Update 08_acrosshost_intro_plot.R

--- 4_EvoSSS_model/08_acrosshost_intro_plot.R
+++ 4_EvoSSS_model/08_acrosshost_intro_plot.R
@@ -12,32 +12,98 @@ posterior_samples <- extract(fit)
 
 beta = posterior_samples$beta
 gamma = posterior_samples$gamma
+R1 = mean(beta/gamma)
+R2 = R1
 I10 = posterior_samples$I10
 I20 = I0 - I10
-hist(beta-gamma)
-
-
+# > mean(I10/I0)
+# [1] 0.3000261
 time_intro1 = log(I10)/(beta-gamma) # - log(ratio)
 time_intro2 = log(I20)/(beta-gamma) # - log(ratio)
 
-delta1 = time_intro2-time_intro1
-hist(delta1)
+delta = mean(time_intro2-time_intro1)
 
 load(file = 'evoSIR.rdata')
-df2 = data.frame()
+
+df = data.frame()
 for (i in 1:9) {
   fit = fitlist[[i]]
   posterior_samples <- extract(fit)
   beta1 = posterior_samples$beta1
   beta2 = posterior_samples$beta2
   gamma = posterior_samples$gamma
+  R1 = mean(beta1/gamma)
+  R2 = mean(beta2/gamma)
   ratio = i/10
   I10 = round(sum(CaseNum[22:24])*ratio)
   I20 = round(sum(CaseNum[22:24])*(1-ratio))
   time_intro1 = log(I10)/(beta1-gamma) # - log(m)
   time_intro2 = log(I20)/(beta2-gamma) # - log(m)
-  df = data.frame(x = i, y = time_intro2-time_intro1)
-  df2 = rbind(df2, df)
+  delta = time_intro1-time_intro2
+  ci_lower = quantile(delta, probs = 0.025, na.rm = T)
+  ci_upper = quantile(delta, probs = 0.975, na.rm = T)
+  
+  df = rbind(df, data.frame(A = R1, B = R2, 
+                            delta = mean(delta), 
+                            group = ratio,
+                            ci_lower = ci_lower,
+                            ci_upper = ci_upper))
 }
-library(ggpubr)
-ggboxplot(df2, x = "x", y = "y")
+
+df$group = as.factor(df$group)
+
+# Bar plot with error bars using ggplot2
+p = ggplot(df, aes(x = group, y = delta, fill = delta)) +
+  geom_bar(stat = "identity", position = position_dodge(), width = 0.7) +
+  geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), 
+                width = 0.2) +
+  theme_minimal() +
+  theme(legend.key.size = unit(0.2, 'cm')) +
+  scale_y_continuous(breaks = c(-10,0,10), labels = c('-10','0','+10')) +
+  geom_hline(yintercept = 0) + 
+  scale_fill_gradient2(low = "#7c615c", mid = "white", high = "#59788e", 
+                       midpoint = 0, breaks = c(-10,0,10), labels = c('-10','0','+10'),
+                       name = expression(Delta*d))
+p
+pdf(paste0("Output/R0_delta.pdf"), width = 4.5, height = 1)
+print(p)
+dev.off()
+pdf(paste0("Output/R0_delta_legend.pdf"), width = 4.5, height = 2)
+print(p)
+dev.off()
+library(ggplot2)
+library(gridExtra)
+
+
+library(reshape2)
+data_melted <- melt(df, 
+                    id.vars = c("group"),
+                    measure.vars = c("A", "B"))
+
+p <- ggplot(data_melted, 
+            aes(x = group, y = variable, 
+                size = value, fill = value)) +
+  geom_point(shape = 21) +
+  scale_size_continuous(range = c(1, 12)) +  # Adjust size range based on your specific needs
+  theme_minimal() +
+  labs(x = "", y = "", size = "R0") +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "top",
+        panel.background = element_blank()) +
+  # scale_x_continuous(breaks = 1:9/10) +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", 
+                       midpoint = 2.5, name = 'R0')
+
+
+# Print the plot
+print(p)
+
+pdf(paste0("Output/R0.pdf"), width = 4, height = 2.2)
+print(p)
+dev.off()
+
+pdf(paste0("Output/R0_legend.pdf"), width = 4.5, height = 2.5)
+print(p)
+dev.off()
+
