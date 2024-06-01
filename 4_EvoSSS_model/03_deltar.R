782a0a040fd02ecae41736c02fd415ea46325a1d
Hc1023 <52774929+Hc1023@users.noreply.github.com>
2024-06-01 11:33:18 +0800

    update

--- 4_EvoSSS_model/03_deltar.R
+++ 4_EvoSSS_model/03_deltar.R
@@ -10,20 +10,21 @@ posterior_samples <- rstan::extract(fit)
 c = posterior_samples$beta1/posterior_samples$beta2
 dat = data.frame(y = -log(c))
 
-p = ggplot(dat, aes(y=y)) + 
-  geom_histogram(binwidth=0.001, color = "#2e6bc5", fill= alpha("skyblue",0.3)) +
-  geom_boxplot(width=800, aes(x = 1500),
+p = ggplot(dat, aes(x=y)) + 
+  geom_histogram(binwidth=0.001, color = "#2e6bc5", 
+                 fill= alpha("skyblue",0.3)) +
+  geom_boxplot(width=800, aes(y = 1500),
                outlier.alpha = 0.1)  +
   theme_bw() +
-  ylab(expression(r[B]-r[A])) + xlab('Counts (1e+3)') +
-  scale_x_continuous(breaks = seq(0,3000, 1000), labels = seq(0,3)) + 
-  scale_y_continuous(limits = range(dat$y), n.breaks = 5,
+  xlab(expression(r[B]-r[A])) + ylab('Counts (1e+3)') +
+  scale_y_continuous(breaks = seq(0,3000, 1000), labels = seq(0,3)) + 
+  scale_x_continuous(limits = range(dat$y), n.breaks = 5,
                      sec.axis = sec_axis(
                        transform = ~ exp(.*(-1)), 
                        name = expression(beta[A]/beta[B])
                      )) 
 p
 
-pdf(file = 'Output/deltar.pdf', width = 2.2, height = 1.6)
+pdf(file = 'Output/deltar.pdf', width = 2.8, height = 1.8)
 print(p)
 dev.off()
