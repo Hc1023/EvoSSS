1b029917870508b0f5fc8781d48d020c0c9bffe3
Hc1023 <52774929+Hc1023@users.noreply.github.com>
2024-06-20 11:27:17 +0800

    update

--- 3_Epidemiological_analysis/12_VOC.R
+++ 3_Epidemiological_analysis/12_VOC.R
@@ -7,41 +7,50 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 library(RColorBrewer)
-dat = readxl::read_excel("Output/metadata/VOC.xlsx")
-table(dat$clade)
-unique(dat$clade)
-dat = dat[!is.na(dat$clade)&!is.na(dat$submission_date),]
 
-df = data.frame(dat[,c('submission_date', 'clade')])
-df$submission_date = as.Date(df$submission_date)
-df$x = format(df$submission_date, '%Y-%m')
-
-levels = c("22B (Omicron)","22A (Omicron)","21M (Omicron)","21L (Omicron)",
-           "21K (Omicron)","21J (Delta)","21I (Delta)", "21H (Mu)","20E (EU1)","21A (Delta)",
-           "20I (Alpha, V1)", "20B","20A" )
-
-VOC = c('Omicron','Delta','Alpha','20A')
-df$V =  NA
-for (i in 1:length(VOC)) {
-  v = VOC[i]
-  df$V[df$clade %in% levels[grep(v,levels)]] = v
+if(F){
+  dat = readxl::read_excel("Output/metadata/VOC.xlsx")
+  table(dat$clade)
+  unique(dat$clade)
+  dat = dat[!is.na(dat$clade)&!is.na(dat$submission_date),]
+  
+  df = data.frame(dat[,c('submission_date', 'clade')])
+  df$submission_date = as.Date(df$submission_date)
+  result <- df %>%
+    group_by(submission_date, clade) %>%
+    summarise(count = n()) %>% as.data.frame()
+  colnames(result)[1] = 'date'
+  levels = c("22B (Omicron)","22A (Omicron)","21M (Omicron)","21L (Omicron)",
+             "21K (Omicron)","21J (Delta)","21I (Delta)", "21H (Mu)","20E (EU1)","21A (Delta)",
+             "20I (Alpha, V1)", "20B","20A" )
+  
+  VOC = c('Omicron','Delta','Alpha','20A')
+  result$V =  NA
+  for (i in 1:length(VOC)) {
+    v = VOC[i]
+    result$V[result$clade %in% levels[grep(v,levels)]] = v
+  }
+  result = result[!is.na(result$V),]
+  result$V[result$V == '20A'] = 'D614G'
+  result = result[,-2]
+  
+  write.csv(result, 'VOC.csv', row.names = F)
 }
-df = df[!is.na(df$V) & !is.na(df$x),]
 
-df$V[df$V == '20A'] = 'D614G'
-result <- df %>%
+result = read.csv('VOC.csv')
+result$date = as.Date(result$date)
+result$x = format(result$date, '%Y-%m')
+df <- result %>%
   group_by(x, V) %>%
-  summarise(count = n()) %>% as.data.frame()
+  summarise(count2 = sum(count)) %>% as.data.frame()
+df$x = as.Date(paste0(df$x,'-01'))
 VOC = c('Omicron','Delta','Alpha','D614G')
-result$x = as.Date(paste0(result$x,'-01'))
-result$V = factor(result$V, levels = VOC) 
+df$V = factor(df$V, levels = VOC)
 
-values = c('#4148f0','#b2360d','#81e880','#fddd10')
-show_col(values)
 values = brewer.pal(n=4,name = "Spectral")
 
 p = ggplot() +
-  geom_area(data = result, aes(x = x, y = count, fill = V),
+  geom_area(data = df, aes(x = x, y = count2, fill = V),
             position = 'fill') +
   scale_fill_manual(values = values) +
   scale_x_date(breaks = seq(as.Date('2020-01-01'), as.Date('2024-11-01'), by="6 months"),
@@ -57,5 +66,5 @@ p
 pdf(paste0("Output/VOC.pdf"), width = 4.2, height = 1.8)
 print(p)
 dev.off()
-library(RColorBrewer)
+
 
--- 3_Epidemiological_analysis/13_VOC_gisaid.R
+++ 3_Epidemiological_analysis/13_VOC_gisaid.R
@@ -18,43 +18,35 @@ if(F){
   
   metadata = metadata[metadata$GISAID_clade %in% c('L','S','G','GRY','GK'),]
   metadata = metadata[!is.na(metadata$date),]
-  metadata$x = format(metadata$date, '%Y-%m')
   result <- metadata %>%
-    group_by(x, GISAID_clade) %>%
+    group_by(date, GISAID_clade) %>%
     summarise(count = n()) %>% as.data.frame()
-  
-  
   write.csv(result, 'VOC_gisaid.csv', row.names = F)
-}
 
-if(F){
-  result = read.csv('VOC_gisaid.csv')
-  VOC = c('D614G', 'A', 'B', 'Delta','Alpha')
-  names(VOC) = unique(result$GISAID_clade)
-  result$V = NA
-  for (i in 1:length(VOC)) {
-    result$V[result$GISAID_clade == names(VOC)[i]] = VOC[i]
-  }
-  result$x = as.Date(paste0(result$x,'-01'))
-  VOC = VOC[c(4,5,1,3,2)]
-  VOC = c("Delta","Alpha","D614G","B","A")
-  result$V = factor(result$V, levels = VOC) 
-  result = result[result$x <= as.Date('2021-10-31'),]
-  write.csv(result, 'VOC_gisaid.csv', row.names = F)
-  
+
 }
 
 result = read.csv('VOC_gisaid.csv')
-VOC = c("Delta","Alpha","D614G","B","A")
-result$V = factor(result$V, levels = VOC)
-result$x = as.Date(result$x)
-values = c('#b2360d','#81e880','#fddd10',hue_pal()(3)[c(3,1)])
-show_col(brewer.pal(n=4,name = "Spectral"))
-brews = brewer.pal(n=4,name = "Spectral")
+result$date = as.Date(result$date)
+result$x = format(result$date, '%Y-%m')
+df <- result %>%
+  group_by(x, GISAID_clade) %>%
+  summarise(count2 = sum(count)) %>% as.data.frame()
+df$x = as.Date(paste0(df$x,'-01'))
+VOC = c("Delta","Alpha","D614G","B 19A","A")
+names(VOC) = c("GK","GRY","G","S","L")
+df = df[df$GISAID_clade %in% names(VOC),]
+df$V = NA
+for (i in 1:length(VOC)) {
+  df$V[df$GISAID_clade == names(VOC)[i]] = VOC[i]
+}
+
+df2 = df[df$x <= as.Date('2021-10-31'),]
+df2$V = factor(df2$V, levels = VOC)
 values = c(brews[2:4], hue_pal()(3)[c(3,1)])
 
 p = ggplot() +
-  geom_area(data = result, aes(x = x, y = count, fill = V),
+  geom_area(data = df2, aes(x = x, y = count2, fill = V),
             position = 'fill') +
   scale_fill_manual(values = values) +
   scale_x_date(breaks = seq(as.Date('2020-01-01'), as.Date('2024-11-01'), by="6 months"),
@@ -68,7 +60,7 @@ p = ggplot() +
         plot.margin = margin(t = 10, r = 10, b = 10, l = 10))
 p
 
-pdf(paste0("Output/VOC_gisaid.pdf"), width = 4.2, height = 1.8)
+pdf(paste0("Output/VOC_gisaid.pdf"), width = 3.5, height = 1.8)
 print(p)
 dev.off()
 
